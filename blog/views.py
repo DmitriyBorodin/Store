5682ea6c9d63b786f68fca6ce2fe3cf24237cf81
Dmitriy Borodin <dmitriyborodinpm@gmail.com>
2024-08-13 00:05:01 +0300

    added view counter, applied filter for blog posts to only show is_published=True, added slug names for titles, changed redirect after edit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, \
     DeleteView
+from pytils.translit import slugify
 
 from blog.models import Blog
 from django.shortcuts import render
@@ -8,6 +9,11 @@ from django.shortcuts import render
 
 class BlogListView(ListView):
     model = Blog
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(is_published=True)
+        return queryset
     # app_name/<model_name>_<action>
     # blog/blog_list.html
 
@@ -15,17 +21,40 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Blog
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class BlogCreateView(CreateView):
     model = Blog
     fields = ('title', 'content', 'blog_preview')
     success_url = reverse_lazy('blog:list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+
+        return super().form_valid(form)
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'content', 'blog_preview')
-    success_url = reverse_lazy('blog:list')
+    # success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("blog:detail", args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
