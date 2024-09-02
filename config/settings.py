daca645b9ce56d65bb818ee97a57760649d7d203
Dmitriy Borodin <dmitriyborodinpm@gmail.com>
2024-09-02 22:11:51 +0300

    added authentication via email and password, verification via token from email, authorization for user and password reset option

--- config/settings.py
+++ config/settings.py
@@ -135,3 +135,20 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 AUTH_USER_MODEL = "users.User"
+
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/"
+
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'Fertizis@yandex.ru'
+EMAIL_HOST_PASSWORD = 'tszombnzzvsliiqi'
+EMAIL_USE_TSL = False
+EMAIL_USE_SSL = True
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# EMAIL_ADMIN = EMAIL_HOST_USER
--- users/views.py
+++ users/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+import secrets
 
-# Create your views here.
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.views import PasswordResetView
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, UpdateView, TemplateView
+
+from catalog.forms import StyleMixin
+from users.forms import UserRegisterForm
+from users.models import User
+
+from config.settings import EMAIL_HOST_USER
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f"http://{host}/users/email-confirm/{token}/"
+        send_mail(
+            subject="Подтверждение почты",
+            message=f"Добрый день, перейдите по ссылке чтобы подтвердить почту {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+        return super().form_valid(form)
+
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse("users:login"))
+
+
+class UserPasswordResetView(PasswordResetView, StyleMixin):
+    form_class = PasswordResetForm
+    template_name = "users/password_reset.html"
+    success_url = reverse_lazy("users:password_reset")
+
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+        try:
+            user = User.objects.get(email=email)
+            if user:
+                password = User.objects.make_random_password(length=8)
+                user.set_password(password)
+                user.save()
+                send_mail(
+                    subject="Ресет пароля",
+                    message=f"Ваш новый пароль - {password}",
+                    from_email=EMAIL_HOST_USER,
+                    recipient_list=[email]
+                )
+            return redirect(reverse('users:login'))
+        except:
+            return redirect(reverse('users:mail_not_found'))
+
+
+class NoMailView(TemplateView):
+    template_name = "users/mail_not_found.html"
+
+
